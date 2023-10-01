0588625efe9ee4fc2ff32ed377e13323715d9e65
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-10-01 14:10:19 +0100

    feat: null safety migration

--- lib/models/stores/stores.dart
+++ lib/models/stores/stores.dart
@@ -1,4 +1,3 @@
-//@dart=2.9
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:kubico/models/users/user_address.dart';
@@ -7,13 +6,13 @@ import 'package:kubico/utils/extensions.dart';
 enum StoreStatus { open, closing, closed }
 
 class Stores {
-  String id;
-  String name;
-  String image;
-  String phone;
-  UserAddress address;
-  Map<String, Map<String, TimeOfDay>> opening;
-  StoreStatus status;
+  String? id;
+  String? name;
+  String? image;
+  String? phone;
+  UserAddress? address;
+  Map<String, Map<String, TimeOfDay>>? opening;
+  StoreStatus? status;
 
   Stores.fromDocument(DocumentSnapshot doc) {
     id = doc.id;
@@ -23,7 +22,7 @@ class Stores {
     address = UserAddress.fromMap(doc.get('address') as Map<String, dynamic>);
     opening = (doc.get('opening') as Map<String, dynamic>).map((key, value) {
       final timeString = value as String;
-      if (timeString != null && timeString.isNotEmpty) {
+      if (timeString.isNotEmpty) {
         final splited = timeString.split(RegExp(r'[:-]'));
         return MapEntry(key, {
           'from': TimeOfDay(
@@ -32,52 +31,51 @@ class Stores {
               hour: int.parse(splited[2]), minute: int.parse(splited[3]))
         });
       } else {
-        return MapEntry(key, null);
+        return MapEntry(key, {});
       }
     });
     updateStatus();
   }
 
-  String get addressText =>
-      '${address.street}, ${address.district}, '
-          ' ${address.city}/${address.province} - ${address.country}';
+  String get addressText => '${address!.street}, ${address!.district}, '
+      ' ${address!.city}/${address!.province} - ${address!.country}';
 
   String get openingText {
-    return 'Seg - Sex: ${formatedPeriod(opening['mondayfriday'])}\n'
-        'Sáb: ${formatedPeriod(opening['saturday'])}\n'
-        'Dom: ${formatedPeriod(opening['sunday'])}';
+    return 'Seg - Sex: ${formatedPeriod(opening!['mondayfriday']!)}\n'
+        'Sáb: ${formatedPeriod(opening!['saturday']!)}\n'
+        'Dom: ${formatedPeriod(opening!['sunday']!)}';
   }
 
-  String get cleanPhone => phone.replaceAll(RegExp(r'[^\d]'), '');
+  String get cleanPhone => phone!.replaceAll(RegExp(r'[^\d]'), '');
 
   String formatedPeriod(Map<String, TimeOfDay> period) {
     if (period == null) {
       return 'Fechada';
     }
-    return '${period['from'].formatedTime()} - ${period['to'].formatedTime()}';
+    return '${period['from']!.formatedTime()} - ${period['to']!.formatedTime()}';
   }
 
   void updateStatus() {
     final weekDay = DateTime.now().weekday;
 
-    Map<String, TimeOfDay> period;
+    Map<String, TimeOfDay>? period;
     if (weekDay >= 1 && weekDay <= 5) {
-      period = opening['mondayfriday'];
+      period = opening!['mondayfriday'];
     } else if (weekDay == 6) {
-      period = opening['saturday'];
+      period = opening!['saturday'];
     } else {
-      period = opening['sunday'];
+      period = opening!['sunday'];
     }
 
     final now = TimeOfDay.now();
 
     if (period == null) {
       status = StoreStatus.closed;
-    } else if ((period['from'].toMinutes() < now.toMinutes()) &&
-        (period['to'].toMinutes() - 15 > now.toMinutes())) {
+    } else if ((period['from']!.toMinutes() < now.toMinutes()) &&
+        (period['to']!.toMinutes() - 15 > now.toMinutes())) {
       status = StoreStatus.open;
-    } else if ((period['from'].toMinutes() < now.toMinutes()) &&
-        (period['to'].toMinutes() > now.toMinutes())) {
+    } else if ((period['from']!.toMinutes() < now.toMinutes()) &&
+        (period['to']!.toMinutes() > now.toMinutes())) {
       status = StoreStatus.closing;
     } else {
       status = StoreStatus.closed;
@@ -88,16 +86,12 @@ class Stores {
     switch (status) {
       case StoreStatus.open:
         return ' Aberta';
-        break;
       case StoreStatus.closing:
         return ' A fechar';
-        break;
       case StoreStatus.closed:
         return ' Fechada';
-        break;
       default:
         return '';
-        break;
     }
   }
 }
